995bb2dc38baba1e9b351ac6d09757dde1e834a8
David Thevenin <david.thevenin@gmail.com>
2012-02-01 18:33:13 +0100

    Add first GUI Tests

--- tests/test.sh
+++ tests/test.sh
@@ -4,8 +4,10 @@ rm -rf temp
 mkdir temp
 jscoverage --no-instrument=jsunit ../lib_debug temp
 cp -R jsunit temp
+cp -R gui temp
 
 export TEST_PATH=`pwd`
 
 echo "<html><head><title>Test</title><meta http-equiv=\"refresh\" content=\"0;url=file://$TEST_PATH/jsunit/testRunner.html?testPage=$TEST_PATH/unit/index.html&autoRun=true\" /></head><body></body></html>" > temp/index.html
 open -a WebKit temp/index.html
+open -a WebKit temp/gui/index.html
