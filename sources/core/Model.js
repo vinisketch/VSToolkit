e1127793c55372d4b1b52f086e54a1c49885cbc0
David Thevenin <david.thevenin@gmail.com>
2013-03-25 17:20:12 +0900

    Remove white space

--- sources/core/Model.js
+++ sources/core/Model.js
@@ -1,17 +1,17 @@
 /**
-  Copyright (C) 2009-2012. David Thevenin, ViniSketch SARL (c), and 
+  Copyright (C) 2009-2012. David Thevenin, ViniSketch SARL (c), and
   contributors. All rights reserved
-  
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published
   by the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU Lesser General Public License for more details.
-  
+
   You should have received a copy of the GNU Lesser General Public License
   along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
@@ -23,11 +23,11 @@
  * @class
  * vs.core.Model is a class that defines the basic Model mechanisms to implement
  * a MVC like architecture. If you need to implement a MVC component, you
- * should extend this class.<br/><br/> >>>> THIS CODE IS STILL UNDER BETA AND 
+ * should extend this class.<br/><br/> >>>> THIS CODE IS STILL UNDER BETA AND
  * THE API MAY CHANGE IN THE FUTURE <<< <p>
  * WikiPedia gives this following definition of a model:<br>
- * "The model manages the behavior and data of the application, responds to 
- * requests for information about its state (usually from the view), and 
+ * "The model manages the behavior and data of the application, responds to
+ * requests for information about its state (usually from the view), and
  * responds to instructions to change state (usually from the controller)"
  * <p>
  * The Model class exposes 2 kinds of mechanisms you will need:
@@ -37,7 +37,7 @@
  * </ul>
  *
  * <p/>
- *  
+ *
  * <p/>
  * The fallowing example show a TodoModel class with three properties
  * @example
@@ -52,20 +52,20 @@
  *     done: vs.core.Object.PROPERTY_IN_OUT,
  *     date: vs.core.Object.PROPERTY_OUT
  *   },
- *  
+ *
  *   // Initialization
  *   initComponent : function ()
  *   {
  *     this._date = new Date ();
  *     this._done = false;
  *     this._content = "";
- *   } 
+ *   }
  * });
  *
  * var myModel = new TodoModel ({content:"Something to do"});
  * myModel.init ();
  *
- * @see vs.core.DataStorage 
+ * @see vs.core.DataStorage
  * @author David Thevenin
  *
  *  @constructor
@@ -90,35 +90,35 @@ Model.prototype = {
   /*****************************************************************
    *
    ****************************************************************/
-   
+
   /**
    * @protected
    * @type {Object}
    */
    __bindings__: null,
-   
+
   /**
    * @protected
    * @type {Array}
    */
    __links__: null,
-   
+
   /**
    * @protected
    * @type {Boolean}
    */
    __should_propagate_changes__: true,
-   
+
   /**
    * @protected
    * @type {vs.core.DataStorage}
    */
    _sync_service_: null,
-  
+
   /*****************************************************************
-   *              
+   *
    ****************************************************************/
-   
+
   /**
    * @protected
    * @function
@@ -126,13 +126,13 @@ Model.prototype = {
   destructor: function ()
   {
     core.Object.prototype.destructor.call (this);
-    
+
     if (this._sync_service_) this._sync_service_.removeModel (this);
-    
+
     function deleteBindings (list_bind)
     {
       if (!list_bind) return;
-      
+
       var bind, l = list_bind.length;
       while (l--)
       {
@@ -140,20 +140,20 @@ Model.prototype = {
         util.free (bind);
       }
     };
-    
+
     for (var spec in this.__bindings__)
     {
       deleteBindings (this.__bindings__ [spec]);
       delete (this.__bindings__ [spec]);
     }
-    
+
     delete (this.__bindings__);
   },
 
   /*****************************************************************
-   *              
+   *
    ****************************************************************/
-  
+
   /**
    * The event bind method to listen model changes
    * <p/>
@@ -168,7 +168,7 @@ Model.prototype = {
    *  myModel.bindChange ('', this, this.onChange);
    *  // Listen all the 'add' change of the model
    *  myModel.bindChange ('add', this, this.onChange);
-   * 
+   *
    * @param {string} action the event specification [optional]
    * @param {vs.core.Object} obj the object interested to catch the event [mandatory]
    * @param {string} func the name of a callback. If its not defined
@@ -186,11 +186,11 @@ Model.prototype = {
     if (!list_bind)
     {
       list_bind = [];
-      this.__bindings__ [spec] = list_bind; 
+      this.__bindings__ [spec] = list_bind;
     }
     list_bind.push (handler);
   },
-  
+
   /**
    *  The event unbind change method
    *  <p>
@@ -198,7 +198,7 @@ Model.prototype = {
    *
    * @name vs.core.Model#unbindChange
    * @function
-   * 
+   *
    * @param {string} spec the event specification [optional]
    * @param {vs.core.Object} obj the object you want unbind [mandatory]
    * @param {string} func the name of a callback. If its not defined
@@ -243,12 +243,12 @@ Model.prototype = {
 
     unbind (this.__bindings__ [spec]);
   },
-  
+
   /**
    * Configure the model to do not propagate event change.<br/>
    * In order to aggregate rapid changes to a model, you will deactivate
    * change event propagate.
-   * After all change are finish you can manual call model.change () to 
+   * After all change are finish you can manual call model.change () to
    * trigger the event.
    * <p>
    * Calling model.change () will reactivate event propagation.
@@ -260,7 +260,7 @@ Model.prototype = {
   {
     this.__should_propagate_changes__ = false;
   },
-  
+
   /**
    *  When you override a Model, you should call this.hasToPropagateChange ()
    *  before calling this.change ().
@@ -275,24 +275,24 @@ Model.prototype = {
   {
     return this.__should_propagate_changes__;
   },
-  
+
   /**
    * Manually trigger the "change" event.
    * If you have deactivated propagation using myModel.stopPropagation ()
-   * in order to aggregate changes to a model, you will want to call 
+   * in order to aggregate changes to a model, you will want to call
    * myModel.change () when you're all finished.
    * <p>
    * Calling myModel.change () reactivate automatic change propagation
    *
    * @name vs.core.Model#change
    * @function
-   * 
+   *
    * @param {String} action the event specification [optional]
    */
   change : function (spec, data, doNotManageLinks)
   {
     var list_bind, event, handler;
-    
+
     this.__should_propagate_changes__ = true;
 
     spec = (spec)? 'change:' + spec : 'change';
@@ -306,29 +306,29 @@ Model.prototype = {
         var l = this.__links__.length, obj;
         while (l--) { this.__links__ [l].configure (this); }
       }
-      
+
       // 2) manage change event propagation
       function _change (list_bind)
       {
         if (!list_bind) return;
         var i = list_bind.length, handler;
-  
+
         while (i--)
         {
           /** @private */
-          handler = list_bind [i];    
-          
+          handler = list_bind [i];
+
           if (handler.func_ptr) // function pointer call
           {
             handler.func_ptr.call (handler.obj, event);
           }
           else if (handler.func) // function name call
           {
-            handler.obj[handler.func] (event); 
+            handler.obj[handler.func] (event);
           }
           else // default notify method
           {
-            handler.obj.notify (event); 
+            handler.obj.notify (event);
           }
         }
       };
@@ -392,9 +392,9 @@ Model.prototype = {
   {
     var df = _df_node_to_def [this._id];
     if (df) { df.propagate (this._id, property); }
-    
+
     if (this.__should_propagate_changes__)
-    { 
+    {
       var l = this.__links__.length, obj;
       if (property) while (l--)
       { this.__links__ [l] [property] = this [property]; }
