fcd628e5e6ac0766cd61477d8b9609dd29b61bfc
David Thevenin <david.thevenin@gmail.com>
2013-03-27 14:51:36 +0900

    Update test

--- tests/test.sh
+++ tests/test.sh
@@ -9,5 +9,5 @@ cp -R gui temp
 export TEST_PATH=`pwd`
 
 echo "<html><head><title>Test</title><meta http-equiv=\"refresh\" content=\"0;url=file://$TEST_PATH/jsunit/testRunner.html?testPage=$TEST_PATH/unit/index.html&autoRun=true\" /></head><body></body></html>" > temp/index.html
-open -a WebKit temp/index.html
-open -a WebKit temp/gui/index.html
+open -a Safari temp/index.html
+open -a Safari temp/gui/index.html
