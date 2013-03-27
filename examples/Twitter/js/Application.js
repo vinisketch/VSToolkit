921426ecd72a1dcafa5158cbbbe5df724ced1354
David Thevenin <david.thevenin@gmail.com>
2013-03-27 15:10:28 +0900

    Add Twitter client sample

--- examples/Twitter/js/Application.js
+++ examples/Twitter/js/Application.js
@@ -1,63 +1,86 @@
 
+
+var TimeLine = vs.core.createClass ({
+
+  /** parent class */
+  parent: vs.core.Array,
+
+  properties : {
+    'screen_name' : vs.core.Object.PROPERTY_IN_OUT,
+    'count' : vs.core.Object.PROPERTY_IN_OUT
+  }
+});
+
 var TheApplication = vs.core.createClass ({
 
   /** parent class */
   parent: vs.ui.Application,
-  
+
+  defaultUserName : 'd_thevenin',
+
+  constructor: function (config)
+  {
+    this._super (config);
+
+    this.data = new vs.core.RestStorage ({
+      url : "https://api.twitter.com/1/",
+      mode : vs.core.RestStorage.JSONP
+    }).init ();
+
+    this.userTimeline = new TimeLine ({
+      screen_name : this.defaultUserName,
+      count: 20
+    }).init ();
+
+    this.data.registerModel ("statuses/user_timeline", this.userTimeline);
+  },
+
   applicationStarted : function (event)
   {
-    this.data = new vs.core.RestStorage ().init ();
-    this.data.url = "https://twitter.com/";
-    
-    this.userTimeline = new vs.core.Array ({id : 'd_thevenin'});
-    this.userTimeline.init ();    
-    
-    this.data.registerModel ("statuses/user_timeline", this.userTimeline);
-
     this.buildMainView ();
-    
+
     this.menu = new MenuPanel ({id: 'MenuPanel'}).init ();
     this.add (this.menu);
     this.menu.hide ();
 
     this.menu.bind ('selectitem', this, this.openCloseMenu);
-    
+
     this.data.load ();
   },
-  
+
   buildMainView : function ()
   {
     this.mainView = new vs.ui.View ().init ();
     this.add (this.mainView);
     this.mainView.setStyles ({width: '100%', height: '100%'});
-    
+
     this.navBar = new vs.ui.NavigationBar ().init ();
     this.mainView.add (this.navBar);
-    
+
     var button = new vs.ui.Button ({
       type: vs.ui.Button.NAVIGATION_TYPE,
-      text: 'settings'
+      text: 'settings',
+      position : [6, 6]
     }).init ();
     this.navBar.add (button);
-    button.position  = [6, 6];
-    
+
     button.bind ('select', this, this.openCloseMenu);
-    
+
     this.list = new vs.ui.List ({id: 'tweets', scroll: true}).init ();
     this.mainView.add (this.list);
-    
+
     this.list.setItemTemplate (TweetView);
     this.list.model = this.userTimeline;
   },
-  
+
   openCloseMenu : function (e)
   {
     if (this.menu.visible) this.menu.hide ();
     else this.menu.show ();
-    
+
     if (e && e.type == 'selectitem')
     {
-      this.userTimeline._id = e.data;
+      this.userTimeline.screen_name = e.data;
       this.data.load ();
     }
   },
@@ -65,7 +88,7 @@ var TheApplication = vs.core.createClass ({
 
 function loadApplication ()
 {
-  new TheApplication ({id:"carrouselBook"}).init ();
+  new TheApplication ({id:"twitter"}).init ();
 
   vs.ui.Application.start ();
 }
