f4fdc0f7195a57e0a811f6a1c6eacc28b3240c1b
David Thevenin <david.thevenin@gmail.com>
2013-04-11 15:15:11 +0900

    Add SVG Test

---
+++ tests/gui/svg.js
@@ -0,0 +1,25 @@
+function launchTest (test_view)
+{
+  var svg = new vs.ui.SVGView ().init ();
+  test_view.appendChild (svg.view);  
+  svg.href = "panel.svg#vis_haut";
+  svg.viewBox = [100, 0, 30, 20];
+
+  svg = new vs.ui.SVGView ().init ();
+  test_view.appendChild (svg.view);
+  svg.position = [0, 50];
+  svg.viewBox = [0, 0, 300, 200];
+  svg.href = "panel.svg";
+
+  svg = new vs.ui.SVGView ().init ();
+  test_view.appendChild (svg.view);
+  svg.position = [0, 350];
+  svg.href = "panel.svg#strobe_on";
+  svg.viewBox = [45, 50, 50, 50];
+
+  svg = new vs.ui.SVGView ().init ();
+  test_view.appendChild (svg.view);
+  svg.position = [60, 350];
+  svg.href = "panel.svg#strobe_off";
+  svg.viewBox = [45, 80, 50, 50];
+}
