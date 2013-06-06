984cae432c99a2d3be722fc0d06975bed7657412
David Thevenin <david.thevenin@gmail.com>
2013-06-06 17:03:38 +0900

    Update tests

--- tests/unit/ui/template_test.js
+++ tests/unit/ui/template_test.js
@@ -26,7 +26,7 @@ function testTemplateApply()
 
 function testViewCompile1 ()
 {
-  var t = '<span style="${style}">name:${lastname},${firstname}</span>';
+  var t = '<span style="${style}" class="huhu">name:${lastname},${firstname}</span>';
   var r = "name:Doo,John"
   var s = "color:blue"
   var template = new vs.ui.Template (t);
@@ -39,6 +39,7 @@ function testViewCompile1 ()
   assertNotNull ('testViewCompile1 1', template);
   assertEquals ('testViewCompile1 2', r, myView.view.innerHTML);
   assertEquals ('testViewCompile1 3', s, myView.view.getAttribute ('style'));
+  assertEquals ('testViewCompile1 3', 'huhu', myView.view.getAttribute ('class'));
 }
 
 function testViewCompile2 ()
@@ -58,11 +59,40 @@ function testViewCompile2 ()
   assertEquals ('testViewCompile2 2', r1, myView.view.firstElementChild.firstElementChild.innerHTML);
   assertEquals ('testViewCompile2 3', r2, myView.view.querySelector ('span').innerHTML);
   assertEquals ('testViewCompile2 4', r3, myView.view.firstElementChild.getAttribute ('style'));
- }
+}
+
+function testViewCompile3 ()
+{
+  var t = '<div>${test}</div>';
+  var template = new vs.ui.Template (t);
+  var myView1 = template.compileView ();
+  var myView2 = template.compileView ();
+  
+  myView1.test = "huhu";
+  myView2.test = "hoho";
+
+  assertNotNull ('testViewCompile3 1', template);
+  assertEquals ('testViewCompile3 2', "huhu", myView1.view.textContent);
+  assertEquals ('testViewCompile3 3', "hoho", myView2.view.textContent);
+}
+
+function testAttributes()
+{
+  var t = '<div style="color:${color};position:${css_pos}"></div>';
+  var template = new vs.ui.Template (t);
+  var myView = template.compileView ();
+  
+  myView.color = "red";
+  myView.css_pos = "absolute";
+ 
+  assertNotNull ('testAttributes 1', template);
+  assertEquals ('testAttributes 2', 'red', myView.view.style.color);
+  assertEquals ('testAttributes 2', 'absolute', myView.view.style.position);
+}
 
 function testTemplateClone ()
 {
-  var t = '<div style="${style}">name:${lastname}<span>,${firstname}</span></div>';
+  var t = '<div style="color:${color}">name:${lastname}<span>,${firstname}</span></div>';
   
   var r1 = "name:Salut1<span>,Gars1</span>"
   var r2 = "name:Salut2<span>,Gars2</span>"
@@ -73,7 +103,7 @@ function testTemplateClone ()
   var view1 = myTemplate.compileView ();
   view1.lastname = "Salut1"
   view1.firstname = "Gars1";
-  view1.style = "display:none";
+  view1.color = "red";
   
   var view2 = view1.clone ();
 
@@ -83,26 +113,32 @@ function testTemplateClone ()
   var view3 = view2.clone ();
 
   view3.lastname = "Salut3"
-  view3.style = "display:block";
+  view3.color = "yellow";
   
   assertNotNull ('testTemplateClone 1', view1);
   assertNotNull ('testTemplateClone 2', view2);
   assertNotNull ('testTemplateClone 3', view3);
   
-  assertEquals ('testTemplateClone 4', "Salut1", view1.lastname);
-  assertEquals ('testTemplateClone 5', "Gars1", view1.firstname);
+  assertEquals ('testTemplateClone 4', "Salut1", view1._lastname);
+  assertEquals ('testTemplateClone 4bis', "Salut1", view1.lastname);
+  assertEquals ('testTemplateClone 5', "Gars1", view1._firstname);
+  assertEquals ('testTemplateClone 5bis', "Gars1", view1.firstname);
   
-  assertEquals ('testTemplateClone 6', "Salut2", view2.lastname);
-  assertEquals ('testTemplateClone 7', "Gars2", view2.firstname);
+  assertEquals ('testTemplateClone 6', "Salut2", view2._lastname);
+  assertEquals ('testTemplateClone 6bis', "Salut2", view2.lastname);
+  assertEquals ('testTemplateClone 7', "Gars2", view2._firstname);
+  assertEquals ('testTemplateClone 7bis', "Gars2", view2.firstname);
   
-  assertEquals ('testTemplateClone 8', "Salut3", view3.lastname);
-  assertEquals ('testTemplateClone 9', "Gars2", view3.firstname);
+  assertEquals ('testTemplateClone 8', "Salut3", view3._lastname);
+  assertEquals ('testTemplateClone 8bis', "Salut3", view3.lastname);
+  assertEquals ('testTemplateClone 9', "Gars2", view3._firstname);
+  assertEquals ('testTemplateClone 9bis', "Gars2", view3.firstname);
 
   assertEquals ('testTemplateClone 10', r1, view1.view.innerHTML);
   assertEquals ('testTemplateClone 11', r2, view2.view.innerHTML);
   assertEquals ('testTemplateClone 12', r3, view3.view.innerHTML);
 
-  assertEquals ('testTemplateClone 13', "display:none", view1.view.getAttribute ('style'));
-  assertEquals ('testTemplateClone 14', "display:none", view2.view.getAttribute ('style'));
-  assertEquals ('testTemplateClone 15', "display:block", view3.view.getAttribute ('style'));
+  assertEquals ('testTemplateClone 13', "red", view1.view.style.color);
+  assertEquals ('testTemplateClone 14', "red", view2.view.style.color);
+  assertEquals ('testTemplateClone 15', "yellow", view3.view.style.color);
 }
