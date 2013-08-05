b2078fdba0a7c09f8f8754b18e04fc17e4eaece8
David Thevenin <david.thevenin@gmail.com>
2013-08-05 18:42:25 +0900

    Add Datalfow tests

---
+++ tests/unit/core/dataflow_test.js
@@ -0,0 +1,251 @@
+function testDataFlowPropagation1 () {
+
+  var TestObject = vs.core.createClass ({
+
+    parent: vs.core.Object,
+
+    properties : {
+      "inOut1": vs.core.Object.PROPERTY_IN_OUT,
+      "inOut2": vs.core.Object.PROPERTY_IN_OUT
+    }
+  });
+
+  var item1 = new TestObject ().init ();
+  var item2 = new TestObject ().init ();
+  var item3 = new TestObject ().init ();
+  
+  var df = vs._default_df_;
+  
+  df.connect (item1, "inOut1", item2, "inOut1")
+  df.connect (item1, "inOut2", item2, "inOut2")
+
+  df.connect (item2, "inOut1", item3, "inOut1")
+  df.connect (item2, "inOut2", item3, "inOut2")
+
+  df.build ();
+
+  df.pausePropagation ();
+  item1['inOut1'] = 1;
+  item1['inOut2'] = 2;
+  df.restartPropagation ();
+  
+  df.propagate ();
+
+  assertEquals ('testDataFlowPropagation1 1', 1, item1._in_out1);
+  assertEquals ('testDataFlowPropagation1 2', 2, item1._in_out2);
+  assertEquals ('testDataFlowPropagation1 3', 1, item2._in_out1);
+  assertEquals ('testDataFlowPropagation1 4', 2, item2._in_out2);
+  assertEquals ('testDataFlowPropagation1 5', 1, item3._in_out1);
+  assertEquals ('testDataFlowPropagation1 6', 2, item3._in_out2);
+}
+
+function testDataFlowPropagation2 () {
+
+  var TestObject = vs.core.createClass ({
+
+    parent: vs.core.Object,
+
+    properties : {
+      "inOut1": vs.core.Object.PROPERTY_IN_OUT,
+      "inOut2": vs.core.Object.PROPERTY_IN_OUT
+    }
+  });
+
+  var item1 = new TestObject ().init ();
+  var item2 = new TestObject ().init ();
+  var item3 = new TestObject ().init ();
+  
+  var df = vs._default_df_;
+  
+  df.connect (item1, ["inOut1", "inOut2"], item2, ["inOut1", "inOut2"]);
+  df.connect (item2, ["inOut1", "inOut2"], item3, ["inOut1", "inOut2"]);
+
+  df.build ();
+
+  df.pausePropagation ();
+  item1['inOut1'] = 1;
+  item1['inOut2'] = 2;
+  df.restartPropagation ();
+  
+  df.propagate ();
+  
+  assertEquals ('testDataFlowPropagation2 1', 1, item1._in_out1);
+  assertEquals ('testDataFlowPropagation2 2', 2, item1._in_out2);
+  assertEquals ('testDataFlowPropagation2 3', 1, item2._in_out1);
+  assertEquals ('testDataFlowPropagation2 4', 2, item2._in_out2);
+  assertEquals ('testDataFlowPropagation2 5', 1, item3._in_out1);
+  assertEquals ('testDataFlowPropagation2 6', 2, item3._in_out2);
+}
+
+function testDataFlowPropagation3 () {
+
+  var TestObject = vs.core.createClass ({
+
+    parent: vs.core.Object,
+
+    properties : {
+      "in": vs.core.Object.PROPERTY_IN,
+      "out": vs.core.Object.PROPERTY_OUT
+    },
+    
+    propertiesDidChange : function () {
+      this._out = this._in + 1;
+    }
+  });
+
+  var item1 = new TestObject ().init ();
+  var item2 = new TestObject ().init ();
+  var item3 = new TestObject ().init ();
+  
+  var df = vs._default_df_;
+  
+  df.connect (item1, ["out"], item2, ["in"]);
+  df.connect (item2, ["out"], item3, ["in"]);
+
+  df.build ();
+
+  item1['in'] = 1;
+  
+  assertEquals ('testDataFlowPropagation3 1', 1, item1._in);
+  assertEquals ('testDataFlowPropagation3 2', 2, item1._out);
+  assertEquals ('testDataFlowPropagation3 3', 2, item2._in);
+  assertEquals ('testDataFlowPropagation3 4', 3, item2._out);
+  assertEquals ('testDataFlowPropagation3 5', 3, item3._in);
+  assertEquals ('testDataFlowPropagation3 6', 4, item3._out);
+}
+
+function testDataFlowFunction1 () {
+
+  var TestObject = vs.core.createClass ({
+
+    parent: vs.core.Object,
+
+    properties : {
+      "inOut": vs.core.Object.PROPERTY_IN_OUT
+    }
+  });
+
+  var item1 = new TestObject ().init ();
+  var item2 = new TestObject ().init ();
+  var item3 = new TestObject ().init ();
+  var item4 = new TestObject ().init ();
+  var item5 = new TestObject ().init ();
+
+  var df = vs._default_df_;
+
+  var foisDeux = function (v) {
+    return v * 2
+  };
+  var foisTrois = function (v) {
+    return v * 3
+  };
+  
+  df.connect (item1, "inOut", item2, "inOut", foisDeux)
+  df.connect (item2, "inOut", item3, "inOut", foisDeux)
+  df.connect (item3, "inOut", item4, "inOut", foisTrois)
+  df.connect (item4, "inOut", item5, "inOut", foisTrois)
+  df.build ();
+
+  item1['inOut'] = 1;
+  
+  assertEquals ('testDataFlowFunction1 1', 1, item1._in_out);
+  assertEquals ('testDataFlowFunction1 2', 2, item2._in_out);
+  assertEquals ('testDataFlowFunction1 3', 4, item3._in_out);
+  assertEquals ('testDataFlowFunction1 4', 12, item4._in_out);
+  assertEquals ('testDataFlowFunction1 5', 36, item5._in_out);
+}
+
+function testDataFlowFunction2 () {
+
+  var TestObject = vs.core.createClass ({
+
+    parent: vs.core.Object,
+
+    properties : {
+      "inOut1": vs.core.Object.PROPERTY_IN_OUT,
+      "inOut2": vs.core.Object.PROPERTY_IN_OUT
+    }
+  });
+
+  var item1 = new TestObject ().init ();
+  var item2 = new TestObject ().init ();
+  var item3 = new TestObject ().init ();
+  
+  var func = function (v1, v2) {
+    return [v2 * 2, v1 * 3];
+  };
+
+  var df = vs._default_df_;
+  
+  df.connect (item1, ["inOut1", "inOut2"], item2, ["inOut1", "inOut2"], func);
+  df.connect (item2, ["inOut1", "inOut2"], item3, ["inOut1", "inOut2"], func);
+
+  df.build ();
+
+  df.pausePropagation ();
+  item1['inOut1'] = 1;
+  item1['inOut2'] = 2;
+  df.restartPropagation ();
+  
+  df.propagate ();
+
+  assertEquals ('testDataFlowFunction2 1', 1, item1._in_out1);
+  assertEquals ('testDataFlowFunction2 2', 2, item1._in_out2);
+  assertEquals ('testDataFlowFunction2 3', 4, item2._in_out1);
+  assertEquals ('testDataFlowFunction2 4', 3, item2._in_out2);
+  assertEquals ('testDataFlowFunction2 5', 6, item3._in_out1);
+  assertEquals ('testDataFlowFunction2 6', 12, item3._in_out2);
+}
+
+function testDataFlowFunction3 () {
+
+  var TestObject1 = vs.core.createClass ({
+
+    parent: vs.core.Object,
+
+    properties : {
+      "inOut1": vs.core.Object.PROPERTY_IN_OUT,
+      "inOut2": vs.core.Object.PROPERTY_IN_OUT
+    }
+  });
+
+  var TestObject2 = vs.core.createClass ({
+
+    parent: vs.core.Object,
+
+    properties : {
+      "inOut": vs.core.Object.PROPERTY_IN_OUT
+    }
+  });
+
+  var item1 = new TestObject1 ().init ();
+  var item2 = new TestObject2 ().init ();
+  var item3 = new TestObject1 ().init ();
+  
+  var func1 = function (v1, v2) {
+    return v1 * v2;
+  };
+  var func2 = function (v) {
+    return [v * 2, v * 3];
+  };
+
+  var df = vs._default_df_;
+  
+  df.connect (item1, ["inOut1", "inOut2"], item2, "inOut", func1);
+  df.connect (item2, "inOut", item3, ["inOut1", "inOut2"], func2);
+
+  df.build ();
+
+  df.pausePropagation ();
+  item1['inOut1'] = 1;
+  item1['inOut2'] = 2;
+  df.restartPropagation ();
+  
+  df.propagate ();
+
+  assertEquals ('testDataFlowFunction3 1', 1, item1._in_out1);
+  assertEquals ('testDataFlowFunction3 2', 2, item1._in_out2);
+  assertEquals ('testDataFlowFunction3 3', 2, item2._in_out);
+  assertEquals ('testDataFlowFunction3 4', 4, item3._in_out1);
+  assertEquals ('testDataFlowFunction3 5', 6, item3._in_out2);
+}
