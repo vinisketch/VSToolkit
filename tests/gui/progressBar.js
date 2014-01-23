c4029e391a8c3296ea092fa3a4da5123498164a6
David Thevenin <david.thevenin@gmail.com>
2014-01-23 19:13:39 +0900

    update test

--- tests/gui/progressBar.js
+++ tests/gui/progressBar.js
@@ -1,21 +1,26 @@
 function launchTest (test_view)
 {
-  var progress1 = new vs.ui.ProgressBar ({position:[50, 50], size:[300, 10]});
+  var index = 62;
+  var progress1 = new vs.ui.ProgressBar ({
+    position:[50, 50], size:[300, 10], index: index
+  });
   progress1.init ();
 
-  var progress2 = new vs.ui.ProgressBar ({position:[50, 80], size:[300, 10]});
+  var progress2 = new vs.ui.ProgressBar ({
+    position:[50, 80],
+    size:[300, 10]
+  });
   progress2.init ();
   progress2.indeterminate = true;
 
   var progress3 = new vs.ui.ProgressBar ({position:[50, 110], size:[100, 10], 
-    range: [50, 80]});
+    range: [50, 80], index: index});
   progress3.init ();
 
   test_view.appendChild (progress1.view);
   test_view.appendChild (progress2.view);
   test_view.appendChild (progress3.view);
 
-  var index = 0;
   function updateProgress ()
   {
     progress1.index = index;
@@ -24,5 +29,5 @@ function launchTest (test_view)
     if (index > 100) index = 0;
   }
   
-  setInterval (updateProgress, 10);
+  vs.scheduleAction (function () {setInterval (updateProgress, 10)}, 1000);
 }
--- tests/gui/slider.js
+++ tests/gui/slider.js
@@ -1,26 +1,26 @@
 function launchTest (test_view)
 {
-  var slider = new vs.ui.Slider ({position:[50, 50], size:[300, 10]});
+  var slider = new vs.ui.Slider ({position:[50, 50], size:[300, 10], value: 64});
   slider.init ();
 
   test_view.appendChild (slider.view);
   slider.refresh ();
 
-  var label1 = new vs.ui.TextLabel ({position:[50, 10], text: '0'});
+  var label1 = new vs.ui.TextLabel ({position:[50, 10], text: '64'});
   label1.init ();
   test_view.appendChild (label1.view);
   
   slider.bind ('continuous_change', this,
     function (e) {label1.text = Math.floor (e.data);});
 
-  slider = new vs.ui.Slider ({position:[50, 100], size:[10, 100], 
+  slider = new vs.ui.Slider ({position:[50, 100], size:[10, 100], value: 56, 
     orientation:vs.ui.Slider.VERTICAL, range: [50, 60]});
   slider.init ();
 
   test_view.appendChild (slider.view);
   slider.refresh ();
   
-  var label2 = new vs.ui.TextLabel ({position:[100, 100], text: '50'});
+  var label2 = new vs.ui.TextLabel ({position:[100, 100], text: '56'});
   label2.init ();
   test_view.appendChild (label2.view);
   
