fe65011f02bee6e6742146b820f6d40ff49127f9
David Thevenin <david.thevenin@gmail.com>
2014-02-14 19:00:44 +0900

    Update libs

--- lib_debug/js/vs_ui.js
+++ lib_debug/js/vs_ui.js
@@ -16385,12 +16385,14 @@ InputField.prototype = {
       else { this._activateDelete (false); }
       
       vs.addPointerListener (document, core.POINTER_START, manageBlur, true);
+      this.propagate ('focus');
     }
     else if (event.type === 'blur')
     {
       this.removeClassName ('focus');
       this._activateDelete (false);
       window.scrollTo (0, 0);
+      this.propagate ('blur');
     }
   }
 }
