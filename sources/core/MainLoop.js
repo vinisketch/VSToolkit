54b9ba21ff6137db30eaf0f3d9b3f6dc17f25c79
David Thevenin <david.thevenin@gmail.com>
2014-06-16 11:13:42 +0900

    setImmediate implementation simplification

--- sources/core/MainLoop.js
+++ sources/core/MainLoop.js
@@ -58,31 +58,6 @@ Handler.release = function (handler) {
   Handler__pool.push (handler);
 }
 
-
-/**
- *  Structure used for managing task
- *  @private
- */
-function TaskHandler (func, args) {
-  this.func_ptr = func;
-  this.args = args;
-}
-
-/**
- * @private
- */
-TaskHandler.prototype.run = function () {
-  this.func_ptr.apply (undefined);
-};
-
-/**
- * @private
- */
-TaskHandler.prototype.destructor = function () {
-  delete (this.args);
-  delete (this.func_ptr);
-};
-
 /**
  * @private
  */
@@ -95,11 +70,11 @@ var
   // Event reference on the current synchronous event.
   _sync_event = null,
   
-  // Actions queue. This array contains all actions (function for the moment)
+  // Actions queue. This array contains all actions (Action object)
   // that have to be execute.
-  // This queue is used only in case we use our own implementation of 
-  // setImmediate.
-  _actions_queue  = [],
+  _main_actions_queue = [],
+  _tmp_actions_queue = [],
+
   // Boolean indicating if we are propagating a event or not.
   // To secure event propagation, in particular to avoid a event pass a previous
   // event, we manage a events queue and block new propagation if a event is
@@ -217,7 +192,7 @@ function doOneEvent (burst, isSynchron) {
     if (isSynchron) doOneHandler (handler_list [i])
   
     else (function (handler) {
-        vs.setImmediate (function () { doOneHandler(handler) });
+        setImmediate (function () { doOneHandler(handler) });
       }) (handler_list [i])
   }
 }
@@ -244,74 +219,9 @@ function doOneSyncEvent () {
   _sync_event = null;
 }
 
-/**
- * doAction, execute one action. This method is called with our setImmediate
- * implementation.
- *
- * @private
- */
-function doAction () {
-
-  if (!_actions_queue.length) return;
-  
-  var action = _actions_queue.shift ();
-
-  if (action) try {
-    _is_action_runing = true;
-    action.run ();
-  }
-  catch (e) {
-    if (e.stack) console.error (e.stack);
-    else console.error (e);
-  }
-
-  vs.util.free (action);
-  _is_action_runing = false;
-
-  if (_actions_queue.length) { _delay_do_action (); }
-}
-
-/**
- * doAction, execute one action. This method is called with our setImmediate
- * implementation.
- *
- * @private
- */
-function installPostMessageImplementation () {
-
-  var MESSAGE_PREFIX = "vs.core.scheduler" + Math.random ();
-
-  function onGlobalMessage (event) {
-    if (event.data === MESSAGE_PREFIX) {
-      doAction ();
-    }
-  }
-  
-  if (window.addEventListener) {
-    window.addEventListener ("message", onGlobalMessage, false);
-  }
-
-  return function () {
-    window.postMessage (MESSAGE_PREFIX, "*");
-  };
-}
-
-var _delay_do_action = (window.postMessage)?installPostMessageImplementation():
-  function () {setTimeout (doAction, 0)};
-
-/**
- * Install our awn setImmediate implementation, if needs
- *
- * @private
- */
-var setImmediate = window.setImmediate || function (func) {
-
-  // push the action to execute into the queue
-  _actions_queue.push (new TaskHandler (func));
-
-  // doAction
-  if (!_is_action_runing) _delay_do_action ();
-};
+/********************************************************************
+      setImmediate Polyfill (based on the Action management)
+*********************************************************************/
 
 /**
  * This method is used to break-up long running operations and run a callback
@@ -325,7 +235,97 @@ var setImmediate = window.setImmediate || function (func) {
  * @name vs.setImmediate 
  * @param {Function} func The action to run
  */
-vs.setImmediate = setImmediate.bind (window);
+vs.setImmediate;
+
+/**
+ * Install our awn setImmediate implementation, if needs
+ *
+ * @private
+ */
+var setImmediate, scheduleDoActions;
+if (!window.setImmediate) {
+
+  var is_actions_are_scheduled = false;
+
+  function queueAction (func, ctx) {
+  
+    _main_actions_queue.push (func);
+  
+    if (!is_actions_are_scheduled) {
+      is_actions_are_scheduled = true;
+      scheduleDoActions ();
+    }
+  }
+
+  /**
+   * @private
+   */
+  function doAllActions () {
+    var queue = _main_actions_queue;
+  
+    is_actions_are_scheduled = false;
+  
+    if (queue.length) {
+    
+      // switch queues
+      _main_actions_queue = _tmp_actions_queue;
+      _tmp_actions_queue = queue;
+    
+      try {
+        // execute actions
+        var i = 0, l = queue.length, func;
+        for (; i < l; i++) {
+          func = queue [i];
+          func.call ();
+        }
+      }
+      catch (e) {
+        if (e.stack) console.error (e.stack);
+        else console.error (e);
+      }    
+      // clean the queue
+      queue.length = 0;
+    }
+  }
+
+  /**
+   * doAction, execute one action. This method is called with our setImmediate
+   * implementation.
+   *
+   * @private
+   */
+  function installPostMessageImplementation () {
+
+    var MESSAGE_PREFIX = "vs.scheduler" + Math.random ();
+
+    function onGlobalMessage (event) {
+      if (event.data === MESSAGE_PREFIX) {
+        doAllActions ();
+      }
+    }
+  
+    if (window.addEventListener) {
+      window.addEventListener ("message", onGlobalMessage, false);
+    }
+
+    return function () {
+      window.postMessage (MESSAGE_PREFIX, "*");
+    };
+  }
+
+  // scheduleDoActions, asynchronously execute pending actions.
+  // if postMessage exists, use it (this is the faster way)
+  // otherwise use traditional setTimeout (f, 0) that add a minimum 4ms delay
+  scheduleDoActions = (window.postMessage)?
+    installPostMessageImplementation ():
+    function () { setTimeout (doAllActions, 0) };
+  
+  window.setImmediate =
+    vs.setImmediate = setImmediate = queueAction.bind (window);
+}
+else {
+  vs.setImmediate = setImmediate = window.setImmediate.bind (window);
+}
 
 /**
  * Mainloop core
@@ -336,7 +336,7 @@ function serviceLoop () {
 
   if (_sync_event) doOneSyncEvent ();
 
-  if ((_async_events_queue.length === 0 && _actions_queue.length === 0) ||
+  if ((_async_events_queue.length === 0 && _main_actions_queue.length === 0) ||
       _is_waiting) return;
 
   function loop () {
@@ -346,12 +346,12 @@ function serviceLoop () {
 
   if (_is_async_events_propagating || _is_sync_events_propagating) {
     // do the loop
-    vs.setImmediate (loop);
+    setImmediate (loop);
     return;
   }
 
   // dispatch an event to observers
-  if (!_is_action_runing && _actions_queue.length) _delay_do_action ();
+  if (_main_actions_queue.length) scheduleDoActions ();
   if (_async_events_queue.length) doOneAsyncEvent ();
 }
 
@@ -422,7 +422,7 @@ function scheduleAction (func, delay) {
   else if (delay === ON_NEXT_FRAME) {
     vs.requestAnimationFrame (func);
   }
-  else vs.setImmediate (func);
+  else setImmediate (func);
 }
 
 /********************************************************************
